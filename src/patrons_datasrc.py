7bd32d41fbdb2fea0096b3d87b82614660d87c6a
Hal Blackburn <hwtb2@cam.ac.uk>
2017-09-14 11:54:06 +0100

    WIP: more hacking

--- src/patrons_datasrc.py
+++ src/patrons_datasrc.py
@@ -1,27 +1,37 @@
 from enum import Enum
+import logging
+from functools import wraps
 
 from django.dispatch import Signal
 
 
+logger = logging.getLogger(__name__)
+
 import_started = Signal()
-record_available = Signal(providing_args=['record'])
-import_failed = Signal()
-import_finished = Signal()
 
 
 ImportType = Enum('ImportType', 'FULL_SYNC PARTIAL_UPDATE')
 
 
-class ImportError(RuntimeError):
+class ImportOperationError(RuntimeError):
     pass
 
+def _notify_handler_robust(operation, method):
+    errors = []
+    for h in operation.handlers:
+        assert hasmethod(h, method)
+        try:
+            getattr(h, method)(operation)
+        except Exception as err:
+            errors.append((h, err))
+            logger.exception('Exception raised from handler\'s '
+                             '{}() method. handler: {}'
+                             .format(method, handler))
+    return errors
 
-def _import_failed(operation, err):
-    # Always notify everyone of the operation failing
-    import_failed.send_robust(operation)
+def _notify_import_failed(operation):
+    return _notify_handler_robust(operation, 'on_import_failed')
 
-    raise ImportError('Failed to send import_started: '
-                          'receiver raised exception') from err
 
 def _first_error(responses):
     '''
@@ -32,20 +42,26 @@ def _first_error(responses):
     return next((resp for (_, resp) in responses
                  if isinstance(resp, Exception)), None)
 
+
 # TODO: can we use x = yield coroutines instead of all these signals?
 #   seems yes, but we should provide a generator which makes a generator
 #   implement some sane interface. e.g. you have to send(None) before sending a
 #   real value to an actual generator, which is an odd detail which shouldn't
 #   be exposed.
 
-def perform_import(operation, records):
+def perform_import(record_type, import_type, records):
+
+    operation = ImportOperation(record_type, import_type)
+
     # Contract: everyone who receives an import_started signal MUST receive
     # either an import_failed or import_finished signal. This is guaranteed
     # through the use of send_robust() to send the starting and ending signals.
-    err = _first_error(import_starting.send_robust(operation))
+    err = _first_error(import_started.send_robust(operation))
     if err is not None:
-        _import_failed('Failed to send import_started: '
-                       'receiver raised exception', err)
+        _notify_import_failed(operation)
+        raise ImportOperationError(
+            'import_started signal receiver raised exception. receiver: {}'
+            .format()) from err
 
     # Iterate manually to more easily determine if an error was from a receiver
     # or the record generator
@@ -54,25 +70,26 @@ def perform_import(operation, records):
     while True:
         try:
             record = next(records_iter)
+            if record is None:
+                raise ValueError('records produced None-valued record')
         except StopIteration:
             break
         except Exception as err:
-            _import_failed('Record generator raised exception', err)
+            _notify_import_failed(operation)
+            raise ImportOperationError(
+                'Record generator raised exception') from err
 
         try:
-            record_available.send(operation, record=record)
+            for h in operation.handlers:
+                h.on_record_available(operation, record)
         except Exception as err:
-            _import_failed()
+            _notify_import_failed(operation)
+            raise ImportOperationError(
+                'handler raised from on_record_available()') from err
 
-    try:
-        for record in records:
-            record_available.send(operation, record=record)
-    except Exception as err:
-        raise ImportError('Failed to send import_started: '
-                          'receiver raised exception') from err
-
-
-
+    errors = _notify_handler_robust(operation, 'on_import_finished')
+    if(errors):
+        raise ImportOperationError('handler raised from on_import_finished()')
 
 
 class ImportOperation(object):
@@ -94,7 +111,12 @@ class ImportOperation(object):
     def import_type(self):
         return self._import_type
 
+    @property
+    def handlers(self):
+        return this._handlers
+
     def attach_handler(self, handler):
+        _validate_import_handler(handler)
         if not handler in self._handlers:
             self._handlers.append(handler)
 
@@ -128,7 +150,22 @@ class ImportRecord(object):
             self.get_ids(), self.get_data())
 
 
+def hasmethod(obj, name):
+    return callable(getattr(obj, name, None))
+
+
+def _validate_import_handler(handler):
+    if not (hasmethod(handler, 'on_record_available') and
+            hasmethod(handler, 'on_import_finished') and
+            hasmethod(handler, 'on_import_failed')):
+        raise ValueError('handler doesn\'t have all ImportOperationHandler '
+                         'methods: {}'.format(handler))
+
+
 class ImportOperationHandler(object):
+    '''
+    An event handler for events triggered during a user data import operation.
+    '''
 
     def on_record_available(self, operation, record):
         pass
@@ -139,3 +176,98 @@ class ImportOperationHandler(object):
     def on_import_finished(self, operation):
         pass
 
+
+class OneOffImportOperationHandler(ImportOperationHandler):
+    '''
+    A ImportOperationHandler which can only receive events for a single
+    operation. Event handler methods throw ValueError if called with an
+    operation other than that provided to the constructor.
+    '''
+    def __init__(self, operation, generator):
+        self._operation = operation
+
+    def _validate_operation_is_ours(self, operation):
+        if self._operation is not operation:
+            raise ValueError('received an import event from an operation '
+                             'other than our own')
+
+    def on_record_available(self, operation, record):
+        super().on_record_available(operation, record)
+        self._validate_operation_is_ours(operation)
+
+    def on_import_failed(self, operation):
+        super().on_import_failed(operation)
+        self._validate_operation_is_ours(operation)
+
+    def on_import_finished(self, operation):
+        super().on_import_finished(operation)
+        self._validate_operation_is_ours(operation)
+
+
+class GeneratorImportOperationHandler(OneOffImportOperationHandler):
+    def __init__(self, operation, generator):
+        super().__init__(operation)
+        self._generator = generator
+        self._generator_primed = False
+
+    def on_record_available(self, operation, record):
+        super().on_record_available(operation, record)
+        assert operation is self.operation
+
+        if not self._generator_primed:
+            self._prime_generator()
+
+        self._generator.send(record)
+
+    def _prime_generator(self):
+        next(self.generator)
+        self._generator_primed = True
+
+
+
+def import_operation_handler(f):
+    '''
+    Implement an ImportOperationHandler using a generator function (which acts
+    as a coroutine).
+
+    f is a generator function which receives import records by yielding.
+
+    @import_operation_handler
+    def handler(import_operation):
+        # setup
+        db = get_db()
+
+        while True:
+            record = yield
+
+            if record is None:
+                break
+
+            save_record_to_db(record, db)
+
+    '''
+    @wraps(f)
+    def wrapper(operation):
+
+    return wrapper
+
+
+def import_operation_handler2(f):
+    '''
+
+    @import_operation_handler2
+    def handler(operation, records)
+        # set up
+        tx = start_db_transaction()
+
+        try:
+            for r in records:
+                save_record_to_db(record, tx)
+        except:
+            tx.roll_back()
+
+        # clean up
+        tx.commit()
+    '''
+    @wraps(f)
+    def import_op_handler
